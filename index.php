52f36b377426c507139a81baf54f769382c2355a
Walter Kimaro <wakyj07@gmail.com>
2019-08-21 22:01:44 +0300

    Update banner, add our fleet section

--- index.php
+++ index.php
@@ -18,41 +18,86 @@
 		</div>
 	</div>
 
-	<section id="sectionServices">
-		<div class="section-wrapper">
-			<div class="layout">
-				<div class="service-item">
-					<img src="assets/img/exploration-drilling.jpg" alt="">
+	<div id="lightRedBgWrapper">
+		<section id="sectionServices">
+			<div class="section-wrapper">
+				<div class="layout">
+					<a href="#" class="service-item">
+						<img src="assets/img/exploration-drilling.jpg" alt="">
 
-					<div class="service-description">
-						<h3>Exploration Drilling</h3>
-						<p>
-							Our agile exploration drilling teams mobilise quickly to meet your timelines...
-						</p>
-					</div>
-				</div>
-				
-				<div class="service-item">
-					<img src="assets/img/blasting.jpg" alt="">
+						<div class="service-description">
+							<h3>Exploration Drilling</h3>
+							<p>
+								Quality core / chip samples
+								<!-- Our agile exploration drilling teams mobilise quickly to meet your timelines... -->
+							</p>
+						</div>
+					</a>
+					
+					<a href="#" class="service-item">
+						<img src="assets/img/blasting.jpg" alt="">
 
-					<div class="service-description">
-						<h3>Drilling &amp; Blasting</h3>
-						<p>
-							As a proudly Tanzanian company which has partnered with global leaders and...
-						</p>
-					</div>
-				</div>
-				
-				<div class="service-item">
-					<img src="assets/img/grade-control.jpg" alt="">
+						<div class="service-description">
+							<h3>Drilling &amp; Blasting</h3>
+							<p>
+								Safe blasting, drilling & demolition
+								<!-- As a proudly Tanzanian company which has partnered with global leaders and... -->
+							</p>
+						</div>
+					</a>
+					
+					<a href="#" class="service-item">
+						<img src="assets/img/grade-control.jpg" alt="">
 
-					<div class="service-description">
-						<h3>Grade Control</h3>
-						<p>
-							RC drilling (in pit grade control) is used in open cut mines to define ore grades ...
-						</p>
-					</div>
+						<div class="service-description">
+							<h3>Grade Control</h3>
+							<p>
+								Right hole, right place, no waste
+								<!-- RC drilling (in pit grade control) is used in open cut mines to define ore grades ... -->
+							</p>
+						</div>
+					</a>
 				</div>
+			</div>
+		</section>
+
+		<section id="aboutSection">
+			<div class="section-wrapper">
+				<p>
+					We operate a <strong>modern fleet of</strong> remote-area drilling, 
+					sample extraction rigs, including  both <strong>track and</strong>
+					<strong>truck mounted rigs</strong> and earthworks equipment.</p>
+			</div>
+		</section>
+
+	</div>
+	
+	<section id="fleetSection">
+		<h2>Our Fleets</h2>
+		<div class="section-wrapper layout wrap">
+			<div>
+				<a href="#" class="fleet-item">
+					<h3>Diamond Drilling</h3>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A laboriosam reiciendis magnam tempore dignissimos! Repudiandae corrupti veniam, tempora aspernatur sequi cupiditate? Adipisci eveniet, rerum quibusdam molestias ratione ipsa nobis est!</p>
+				</a>
+			</div>
+			<div>
+				<a href="#" class="fleet-item">
+					<h3>Reverse Circulation Drilling</h3>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A laboriosam reiciendis magnam tempore dignissimos! Repudiandae corrupti veniam, tempora aspernatur sequi cupiditate? Adipisci eveniet, rerum quibusdam molestias ratione ipsa nobis est!</p>
+				</a>
+			</div>
+			<div>
+				<a href="#" class="fleet-item">
+					<h3>Air Core/ RAB / Auger Drilling</h3>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A laboriosam reiciendis magnam tempore dignissimos! Repudiandae corrupti veniam, tempora aspernatur sequi cupiditate? Adipisci eveniet, rerum quibusdam molestias ratione ipsa nobis est!</p>
+				</a>
+			</div>
+			<div>
+				<a href="#" class="fleet-item">
+					<h3>Mine Production Rigs</h3>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A laboriosam reiciendis magnam tempore dignissimos! Repudiandae corrupti veniam, tempora aspernatur sequi cupiditate? Adipisci eveniet, rerum quibusdam molestias ratione ipsa nobis est!</p>
+				</a>
 			</div>
 		</div>
 	</section>
