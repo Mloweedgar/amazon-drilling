a8a6e2ac8a641df09b0390ec3668e76e648f5d0e
Walter Kimaro <wakyj07@gmail.com>
2019-08-28 10:00:30 +0300

    Improve look of Fleet section, add adnvatages section

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
 
 <main>
 	<div id="banner">
-		<img class="slideshow-image active" src="assets/img/banner.jpg" alt="">
-
 		<div class="section-wrapper">
 			<div class="text layout vertical center-center">
 				<h2 id="slideshowContent" class="text-center">
@@ -61,12 +59,62 @@
 			</div>
 		</section>
 
-		<section id="aboutSection">
+		<!-- <section id="aboutSection">
 			<div class="section-wrapper">
 				<p>
 					We operate a <strong>modern fleet of</strong> remote-area drilling, 
 					sample extraction rigs, including  both <strong>track and</strong>
 					<strong>truck mounted rigs</strong> and earthworks equipment.</p>
+			</div>
+		</section> -->
+		
+		<section id="advantagesSection">
+			<div class="section-wrapper layou start">
+				<h2>
+					Why Choose Us
+				</h2>
+	
+				<div class="flex layout wrap">
+					<div class="advantage-item layout center wrap">
+						<div class="icon layout center-center">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-award"><circle cx="12" cy="8" r="7"></circle><polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline></svg>
+						</div>
+						<h3 class="flex">
+							<small>4 Years of</small>
+							proven trust
+						</h3>
+					</div>
+					
+					<div class="advantage-item layout center wrap">
+						<div class="icon layout center-center">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-compass"><circle cx="12" cy="12" r="10"></circle><polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon></svg>
+						</div>
+						<h3 class="flex">
+							<small>Leaders in</small>
+							exploration mining
+						</h3>
+					</div>
+					
+					<div class="advantage-item layout center wrap">
+						<div class="icon layout center-center">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-briefcase"><rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect><path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path></svg>
+						</div>
+						<h3 class="flex">
+							<small>Excellent</small>
+							Mining Services
+						</h3>
+					</div>
+					
+					<div class="advantage-item layout center wrap">
+						<div class="icon layout center-center">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-anchor"><circle cx="12" cy="5" r="3"></circle><line x1="12" y1="22" x2="12" y2="8"></line><path d="M5 12H2a10 10 0 0 0 20 0h-3"></path></svg>
+						</div>
+						<h3 class="flex">
+							<small>Unbeatable</small>
+							Mining Expertise
+						</h3>
+					</div>
+				</div>
 			</div>
 		</section>
 
@@ -74,30 +122,67 @@
 	
 	<section id="fleetSection">
 		<h2>Our Fleets</h2>
-		<div class="section-wrapper layout wrap">
-			<div>
-				<a href="#" class="fleet-item">
-					<h3>Diamond Drilling</h3>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A laboriosam reiciendis magnam tempore dignissimos! Repudiandae corrupti veniam, tempora aspernatur sequi cupiditate? Adipisci eveniet, rerum quibusdam molestias ratione ipsa nobis est!</p>
-				</a>
-			</div>
-			<div>
-				<a href="#" class="fleet-item">
-					<h3>Reverse Circulation Drilling</h3>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A laboriosam reiciendis magnam tempore dignissimos! Repudiandae corrupti veniam, tempora aspernatur sequi cupiditate? Adipisci eveniet, rerum quibusdam molestias ratione ipsa nobis est!</p>
-				</a>
-			</div>
-			<div>
-				<a href="#" class="fleet-item">
-					<h3>Air Core/ RAB / Auger Drilling</h3>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A laboriosam reiciendis magnam tempore dignissimos! Repudiandae corrupti veniam, tempora aspernatur sequi cupiditate? Adipisci eveniet, rerum quibusdam molestias ratione ipsa nobis est!</p>
-				</a>
-			</div>
-			<div>
-				<a href="#" class="fleet-item">
-					<h3>Mine Production Rigs</h3>
-					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A laboriosam reiciendis magnam tempore dignissimos! Repudiandae corrupti veniam, tempora aspernatur sequi cupiditate? Adipisci eveniet, rerum quibusdam molestias ratione ipsa nobis est!</p>
-				</a>
+		<p>
+			We operate a modern fleet of remote-area drilling, sample extraction rigs, including both track and truck mounted rigs and earthworks equipment.
+		</p>
+		<div class="section-wrapper">
+			<div id="fleetScroller" class="layout">
+				<div>
+					<a href="#" class="fleet-item">
+						<div class="image">
+							<img src="https://amazondrilling.com/assets/images/fleets/atlas.jpg" alt="">
+						</div>
+						<div class="fleet-text">
+							<h3>Diamond Drilling</h3>
+							<p>Number of drills: 3</p>
+							<ul>
+								<li>Depth Capacity PQ: 400M - 475M</li>
+								<li>Depth Capacity HQ: 800M - 850M</li>
+								<li>Depth Capacity NQ: 1200M - 1400M</li>
+							</ul>
+						</div>
+					</a>
+				</div>
+				<div>
+					<a href="#" class="fleet-item">
+						<div class="image">
+							<img src="https://amazondrilling.com/assets/images/fleets/srd.jpg" alt="">
+						</div>
+						<div class="fleet-text">
+							<h3>Reverse Circulation Drilling</h3>
+							<p>Number of drills: 1</p>
+							<ul>
+								<li>Depth Capacity PQ: 400M</li>
+								<li>Depth Capacity HQ: 1000M</li>
+								<li>Depth Capacity NQ: 1500M</li>
+							</ul>
+						</div>
+					</a>
+				</div>
+				<div>
+					<a href="#" class="fleet-item">
+						<div class="image">
+							<img src="https://amazondrilling.com/assets/images/fleets/landcruiser.jpg" alt="">
+						</div>
+						<div class="fleet-text">
+							<h3>Air Core/ RAB / Auger Drilling</h3>
+							<p>Number of drills: 1</p>
+							<p>Carrier Type : 6 x 6 Landcruiser Truck</p>
+						</div>
+					</a>
+				</div>
+				<div>
+					<a href="#" class="fleet-item">
+						<div class="image">
+							<img src="https://amazondrilling.com/assets/images/fleets/atlas-copco.jpg" alt="">
+						</div>
+						<div class="fleet-text">
+							<h3>Mine Production Rigs</h3>
+							<p>Number of drills: 2</p>
+							<p>Carrier Type : 6 x 6 Landcruiser Truck</p>
+						</div>
+					</a>
+				</div>
 			</div>
 		</div>
 	</section>
