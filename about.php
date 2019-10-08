625656286d57281f7e8347e0109c7a1fdf50564f
Walter Kimaro <wakyj07@gmail.com>
2019-10-08 16:30:16 +0300

    Added about us page ya kizushi

---
+++ about.php
@@ -0,0 +1,85 @@
+<?php
+	$page = 'about';
+	include('includes/header.php');
+?>
+
+<main>
+	<div id="aboutUsWrapper">
+		<section id="sectionBanner">
+			<div class="section-wrapper">
+                <h1>About Us</h1>
+                <p>
+                    Amazon Drilling is a drilling specialist and mine earthworks contractor based in Tanzania.
+                </p>
+				<p>
+                    We operate a modern fleet of remote-area drilling, sample extraction rigs, including both track and truck mounted rigs and earthworks equipment.
+                </p>
+                <p>
+                    Our all-wheel drive support trucks, transport vehicles and off-road caravans, allow us to offer a complete service to the mining and exploration companies of East and Central Africa
+				</p>
+			</div>
+		</section>
+
+		<section id="sectionMissionVision">
+			<div class="section-wrapper">
+				<div class="layout vertical">
+					<div>
+						<h1>Our Vision</h1>
+						<p class="subtitle">
+                            To be your first choice for professional drilling, mining services and technical expertise in Africa as wholly
+						</p>
+					</div>
+				</div>
+			</div>
+        </section>
+        
+        <section id="sectionMission">
+            <div class="section-wrapper">
+                <div>
+                    <h1>Our Mission</h1>
+                    <p class="subtitle">
+                        To achieve a successful future for our clients, employees and owners by leading our industry in quality and value of service. Our most valuable ‘Assets’, are our people. And our ‘Commitment’ is to consistently deliver customer satisfaction; provide ‘Excellence’ in whatever we do and most of all to give our clients, ‘Value for their money’.
+                    </p>
+                </div>
+            </div>
+        </section>
+		
+		<section id="whyUs">
+			<div class="section-wrapper">
+				<h1>Why Choose Us</h1>
+				<p>
+					<ul>
+						<li>
+                            34 Years of Proven Trust
+						</li>
+						<li>
+                            Leaders in 10+ industries
+						</li>
+						<li>
+                            Excellent Products & Services
+						</li>
+						<li>
+                            15 Companies under the roof
+						</li>
+					</ul>
+				</p>
+			</div>
+		</section>
+		
+		<section id="secretariet">
+			<div class="section-wrapper">
+				<h1>Quality Management</h1>
+				<p>
+                    We at Amazon Drilling constantly strive to create high-quality cost effective services to customers, with a note in mind of dynamic business environment facing mining sector.
+                </p>
+                <p>
+                    We have well-equipped fleets of machineries, plants and team of qualified professionals and experienced drilling crew, operators, engineers to make sure that the services are going as per the standards.
+				</p>
+			</div>
+		</section>
+	</div>
+</main>
+
+<?php
+	include('includes/footer.php');
+?>
